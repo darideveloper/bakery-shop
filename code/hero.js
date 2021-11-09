0492e05b3787012da06f566484abc412658d1924
dari <hernandezdarifrancisco@gmail.com>
2021-11-08 21:00:42 -0600

    Change dots

--- code/hero.js
+++ code/hero.js
@@ -5,6 +5,7 @@ const dot_2 = document.querySelector (".dot-wrapper:nth-child(2)")
 const dot_3 = document.querySelector (".dot-wrapper:nth-child(3)")
 const cover_bg = document.querySelector (".cover > .background")
 var cover_imgs = document.querySelectorAll (".cover > .background img")
+var dots = document.querySelectorAll (".dot-wrapper > .dot")
 
 var last_image = 2
 var current_image = 2
@@ -12,37 +13,48 @@ var running_transition = false
 
 function start_transition () {
     // Set class to cover background for start transition
-    if (current_image != last_image) {
-        cover_bg.classList.add ("transition")
-    }
+    cover_bg.classList.add ("transition")
 } 
 
 function change_image () {
-    // Show only current image
+    // Show only current image and update dots
 
+    // Hide all images
     for (index in Array.from (cover_imgs)) {
         current_img = cover_imgs[index]
         current_img.classList.remove ("active")
     }
 
+    // Show current image
     active_image = cover_imgs[current_image - 1]
     active_image.classList.add ("active")
+
+    // Hide all images
+    for (index in Array.from (dots)) {
+        current_dot = dots[index]
+        current_dot.classList.remove ("active")
+    }
+
+    // Show current image
+    active_dot = dots[current_image - 1]
+    active_dot.classList.add ("active")
     
 }
 
 function end_transition () {
     // Set class to cover background for start transition
-    if (current_image != last_image) {
-        cover_bg.classList.remove ("transition")
-    }
+    cover_bg.classList.remove ("transition")
 }
 
 function update_image () {
     // Update image in the page
-    console.log (current_image)
-    start_transition ()   
-    setTimeout (change_image, 500) 
-    setTimeout (end_transition, 550)
+
+    if (current_image != last_image) {
+        console.log (current_image)
+        start_transition ()   
+        setTimeout (change_image, 500) 
+        setTimeout (end_transition, 550)
+    }
 }
 
 button_next.addEventListener ("click", function (e) {
